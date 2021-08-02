1115fca921566b61b7a89e9d5fff8c0a6c420c78
Kaique henrique <kaiquehenrique777.skrrr@gmail.com>
2021-08-02 15:19:48 -0300

    Adding dotenv to SendController

--- dist/src/controllers/SendController.js
+++ dist/src/controllers/SendController.js
@@ -1,4 +1,6 @@
 "use strict";
+const env = require('dotenv');
+env.config();
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
