c4ad4092d0fd4de96b0780ff5a0add741524dcdd
Kaique henrique <kaiquehenrique777.skrrr@gmail.com>
2021-08-02 11:09:50 -0300

    Adding home route

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import SendController from './controllers/SendController';
 
 const route = Router();
 
+route.get('/', (req: Request, res: Response) => {
+  res.status(200).json({ message: 'Seja bem-vindo!' });
+});
 route.post('/send', SendController.sendEmail);
 
 export default route;
